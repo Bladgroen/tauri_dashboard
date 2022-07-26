a80d5acfdb165f08f379aab146f89082ab8f7c69
Vincent Laureys <vincent.laureys@student.odisee.be>
2022-07-26 11:23:55 +0200

    added live timer

--- src/components/CHeader.vue
+++ src/components/CHeader.vue
@@ -1,9 +1,11 @@
 <template>
-  <div></div>
+  <div>
+    <p>{{ state.currentDate }}</p>
+  </div>
 </template>
 
 <script lang="ts" setup>
-import { ref, reactive } from "vue";
+import { ref, reactive, onBeforeUnmount } from "vue";
 
 let currentDate = ref("");
 let state = reactive({
@@ -18,13 +20,25 @@ const options = {
   hour: "numeric",
   minute: "numeric",
 };
-
-//TODO per minuut laten afgaan & de "om" weglaten
-setInterval(() => {
+let calculateMinute = 60000 - (new Date().getTime() % 60000);
+function formatDate() {
   let today = new Date();
-  //let exactMinute = 60000 - (today.getTime() % 6000)
-  state.currentDate = today.toLocaleDateString("nl", options).replace(/,/g, "");
-});
+
+  state.currentDate = today
+    .toLocaleDateString("nl", options)
+    .replace(/,|om/g, "");
+}
+formatDate();
+
+setTimeout(function () {
+  setInterval(formatDate, 60000);
+  formatDate();
+}, calculateMinute);
+
+// ! Geeft unexpected token error
+// onBeforeUnmount(() => {
+//   clearInterval()
+// }),
 </script>
 
 <style lang=""></style>
